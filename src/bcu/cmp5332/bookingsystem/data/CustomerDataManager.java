7d81d3ecd384f61cc0b665c6be20c595474f399b
Vishalji ODEDRA <vishaljiodedra308@gmail.com>
2022-12-27 21:44:16 +0000

    CustomerDataManager.java updated to v1.0.1
    updated loadData and sortData method of the CustomerDataManager.

--- src/bcu/cmp5332/bookingsystem/data/CustomerDataManager.java
+++ src/bcu/cmp5332/bookingsystem/data/CustomerDataManager.java
@@ -12,10 +12,37 @@ public class CustomerDataManager implements DataManager {
     @Override
     public void loadData(FlightBookingSystem fbs) throws IOException, FlightBookingSystemException {
         // TODO: implementation here
+        try (Scanner sc = new Scanner(new File(RESOURCE))){
+    		int line_idx = 1;
+    		while (sc.hasNextLine()) {
+    			String line = sc.nextLine();
+    			String[] properties1 = line.split(SEPARATOR, -1);
+    			try {
+    				int id = Integer.parseInt(properties1[0]);
+    				String customerName = properties1[1];
+    				String customerNumber = properties1[2];
+    				Customer c1 = new Customer(id, customerName, customerNumber);
+    				fbs.addCustomer(c1);
+    			} catch (NumberFormatException ex) {
+    				throw new FlightBookingSystemException("Unable to parse book id " + properties1[0] + " on line " + line_idx
+                            + "\nError: " + ex);
+    			}
+    			line_idx++;
+    		}
+    	}
     }
 
     @Override
     public void storeData(FlightBookingSystem fbs) throws IOException {
         // TODO: implementation here
+        try (PrintWriter out = new PrintWriter(new FileWriter(RESOURCE))){
+    		for (Customer c1 : fbs.getCustomers()) {
+    			out.print(c1.getId() + SEPARATOR);
+    			out.print(c1.getName() + SEPARATOR);
+    			out.print(c1.getNumber() + SEPARATOR);
+    			out.println();
+    		}
+    	}
+    }
     }
 }
