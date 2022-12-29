b9ad72127fa572b580e312ef1f6cfe9b87818601
Lucifer007XD <115782887+Lucifer007XD@users.noreply.github.com>
2022-12-29 14:08:29 +0000

    Update Booking.java
    Added Getters and Setters

--- src/bcu/cmp5332/bookingsystem/model/Booking.java
+++ src/bcu/cmp5332/bookingsystem/model/Booking.java
@@ -11,9 +11,32 @@ public class Booking {
 
     public Booking(Customer customer, Flight flight, LocalDate bookingDate) {
         // TODO: implementation here
+    	this.customer=customer;
+    	this.flight=flight;
+    	this.bookingDate=bookingDate;
         
     }
+    public void setCustomer(Customer cst) {
+    	this.customer=cst;
+    	
+    }
+    public void setFlight(Flight flg) {
+    	this.flight=flg;
+    }
+    public void setBookingDate(LocalDate bkdt) {
+    	this.bookingDate=bkdt;
+    	
+    }
     
-    // TODO: implementation of Getter and Setter methods
+    public Customer getCustomer() {
+    	return this.customer;
+    }
+    public Flight getFlight() {
+    	return this.flight;
+    }
+    public LocalDate getBookingDate() {
+    	return this.bookingDate;
+    }
     
+        
 }
