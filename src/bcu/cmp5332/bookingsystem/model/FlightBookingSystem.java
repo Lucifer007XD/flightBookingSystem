c7dc301c64f6264f9ba4f4f79772eb5755db22ad
Lucifer007XD <115782887+Lucifer007XD@users.noreply.github.com>
2022-12-29 14:13:20 +0000

    Update FlightBookingSystem.java
    Added All the methods just have to add email check functionality in add Customers

--- src/bcu/cmp5332/bookingsystem/model/FlightBookingSystem.java
+++ src/bcu/cmp5332/bookingsystem/model/FlightBookingSystem.java
@@ -19,6 +19,10 @@ public class FlightBookingSystem {
         List<Flight> out = new ArrayList<>(flights.values());
         return Collections.unmodifiableList(out);
     }
+    public List<Customer> getCustomer(){
+    	List<Customer> out=new ArrayList<>(customers.values());
+    	return Collections.unmodifiableList(out);
+    }
 
     public Flight getFlightByID(int id) throws FlightBookingSystemException {
         if (!flights.containsKey(id)) {
@@ -28,8 +32,10 @@ public class FlightBookingSystem {
     }
 
     public Customer getCustomerByID(int id) throws FlightBookingSystemException {
-        // TODO: implementation here
-        return null;
+        if (!customers.containsKey(id)) {
+        	throw new FlightBookingSystemException("There is no Passenger With that ID.");
+        }
+        return customers.get(id);
     }
 
     public void addFlight(Flight flight) throws FlightBookingSystemException {
@@ -46,7 +52,13 @@ public class FlightBookingSystem {
         flights.put(flight.getId(), flight);
     }
 
-    public void addCustomer(Customer customer) {
-        // TODO: implementation here
-    }
+    public void addCustomer(Customer customer)throws IllegalArgumentException {
+        if(customers.containsKey(customer.getId())) {
+        	throw new IllegalArgumentException("Duplicate Passenger ID.");
+        	
+        }
+        customers.put(customer.getId(), customer);
+        }
+    
+        
 }
