aec3a0bfd7c7c421cce46a78e2037901d56678e0
Lucifer007XD <115782887+Lucifer007XD@users.noreply.github.com>
2023-01-04 23:36:48 +0000

    Update BookingDataManager.java
    some changes for loaddata method

--- src/bcu/cmp5332/bookingsystem/data/BookingDataManager.java
+++ src/bcu/cmp5332/bookingsystem/data/BookingDataManager.java
@@ -27,21 +27,16 @@ public class BookingDataManager implements DataManager {
     			String[] proper = line.split(SEPARATOR, -1);
     			try {
     				int  cusID = Integer.parseInt(proper[0]);
-    				String cusName = proper[1];
-    				String cusPhone = proper[2];
-    				String cusEmail = proper[3];
-    				int flightId = Integer.parseInt(proper[4]);
-    				String flightNum = proper[5];
-    				String origin = proper[6];
-    				String des = proper[7];
-    				LocalDate depDate = LocalDate.parse(proper[8]);
-    				Flight f1 = new Flight(flightId, flightNum, origin, des, depDate);
-    				Customer c1 = new Customer(cusID, cusName, cusPhone, cusEmail);
-    				LocalDate date = LocalDate.parse(proper[9]);
-    				Booking b1 = new Booking(c1, f1, date);
+    				
+    				int flightId = Integer.parseInt(proper[1]);
+    				Flight f1 = fbs.getFlightByID(flightId);
+    				Customer c1 =fbs.getCustomerByID(cusID);
+    				LocalDate date = LocalDate.parse(proper[2]);
+    				Booking b1 =new Booking(c1, f1, date);
     				c1.addBooking(b1);
+					f1.addPassenger(c1);
     			} catch (NumberFormatException ex) {
-    				throw new FlightBookingSystemException("Unable to parse book id " + proper[0] + " on line " + line_id
+    				throw new FlightBookingSystemException("Unable to parse customer id " + proper[0] + " on line " + line_id
                             + "\nError: " + ex);
         		}
     			line_id++;
@@ -58,17 +53,14 @@ public class BookingDataManager implements DataManager {
                     Customer customer=b1.getCustomer();
                     Flight flight=b1.getFlight();
                     out.print(customer.getId() + SEPARATOR);
-    			    out.print(customer.getName() + SEPARATOR);
-    			    out.print(customer.getPhone() + SEPARATOR);
-    			    out.print(customer.getEmail() + SEPARATOR);
+    			   
                     out.print(flight.getId()+SEPARATOR);
-                    out.print(flight.getFlightNumber()+SEPARATOR);
-                    out.print(flight.getOrigin()+SEPARATOR);
-                    out.print(flight.getDestination()+SEPARATOR);
-                    out.print(flight.getDepartureDate()+SEPARATOR);
+                    
                     out.print(b1.getBookingDate()+SEPARATOR);
     			    out.println();
+				}
     		}
     	}
     }
 }
+
