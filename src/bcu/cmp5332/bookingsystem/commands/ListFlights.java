6d7c9a12f46690e71afb3d1e6d1b2521dde2b2a7
Lucifer007XD <115782887+Lucifer007XD@users.noreply.github.com>
2023-01-06 23:14:45 +0000

    Update ListFlights.java

--- src/bcu/cmp5332/bookingsystem/commands/ListFlights.java
+++ src/bcu/cmp5332/bookingsystem/commands/ListFlights.java
@@ -4,16 +4,23 @@ import bcu.cmp5332.bookingsystem.main.FlightBookingSystemException;
 import bcu.cmp5332.bookingsystem.model.Flight;
 import bcu.cmp5332.bookingsystem.model.FlightBookingSystem;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class ListFlights implements Command {
+	LocalDate today=LocalDate.now();
 
     @Override
     public void execute(FlightBookingSystem flightBookingSystem) throws FlightBookingSystemException {
         List<Flight> flights = flightBookingSystem.getFlights();
+        int size=0;
         for (Flight flight : flights) {
-            System.out.println(flight.getDetailsShort());
+        	if(flight.getDepartureDate().isAfter(today)||flight.getDepartureDate().isEqual(today)) {
+        		 System.out.println(flight.getDetailsShort());
+                 size+=1;
+        	}
         }
-        System.out.println(flights.size() + " flight(s)");
+       
+        System.out.println(size+ " flight(s)");
     }
 }
