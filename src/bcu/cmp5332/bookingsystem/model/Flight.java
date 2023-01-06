30f1281739bba921adb9354301fadd0b7f5dbf92
Lucifer007XD <115782887+Lucifer007XD@users.noreply.github.com>
2023-01-06 23:19:59 +0000

    Update Flight.java

--- src/bcu/cmp5332/bookingsystem/model/Flight.java
+++ src/bcu/cmp5332/bookingsystem/model/Flight.java
@@ -15,17 +15,49 @@ public class Flight {
     private String origin;
     private String destination;
     private LocalDate departureDate;
+    private double price;
+    private int capacity;
+    private boolean hidden=false;
 
     private final Set<Customer> passengers;
 
-    public Flight(int id, String flightNumber, String origin, String destination, LocalDate departureDate) {
+    public Flight(int id, String flightNumber, String origin, String destination, LocalDate departureDate, double price,int capacity) {
         this.id = id;
         this.flightNumber = flightNumber;
         this.origin = origin;
         this.destination = destination;
         this.departureDate = departureDate;
+       
+        this.capacity=capacity;
         
         passengers = new HashSet<>();
+    }
+    public boolean getStatus() {
+    	return this.hidden;
+    }
+    public  void setStatus(boolean b) {
+    	this.hidden=b;
+    	
+    }
+    public int getNoOfSeats(){
+    	return this.capacity;
+    }
+    public void setNoOfSeats(int cap) {
+    	this.capacity=cap;
+    }
+    public double getPrice() {
+    	int leftSeats=this.capacity-passengers.size();
+    	if(leftSeats<=(0.05*this.capacity)) {
+    		this.price=this.price+(0.1*this.price);
+    	}else if(leftSeats<=(0.1*this.capacity)) {
+    		this.price=this.price+(0.06*this.price);
+    	}else if(leftSeats<=(0.2*this.capacity)) {
+    		this.price=this.price+(0.02*this.price);
+    		}
+    	return this.price;
+    }
+    public void setPrice(double price) {
+    	this.price=price;
     }
 
     public int getId() {
@@ -75,15 +107,20 @@ public class Flight {
     public String getDetailsShort() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/YYYY");
         return "Flight #" + id + " - " + flightNumber + " - " + origin + " to " 
-                + destination + " on " + departureDate.format(dtf);
+                + destination + " on " + departureDate.format(dtf)+" - Price: $"+this.getPrice();
     }
-
+   
     public String getDetailsLong() {
+    	int leftSeats=this.capacity-this.passengers.size();
+    	
         String flightDetails="Flight #"+this.id+"\n"+
     "Flight No.: "+this.flightNumber+"\n"+
         		"Origin: "+this.origin+"\n"+
     "Destination: "+this.destination+"\n"+
-        		"Departure Date: "+this.departureDate;
+        		"Departure Date: "+this.departureDate+"\n"+
+    "Price: "+this.getPrice()+"\n"+
+        		"Total No. of Seats: "+this.capacity+"\n"+
+    "No. of Seats Left: "+leftSeats;
         String passengerDetails="";
         for(Customer p:passengers) {
         	passengerDetails=passengerDetails+"\n"+"* Id: "+p.getId()+" - "+p.getName()+" - "+p.getPhone();
@@ -99,7 +136,12 @@ public class Flight {
         	}
         	
         }
+        if(passengers.size()>=this.capacity) {
+        	 throw new FlightBookingSystemException("Seats Not Available Flight is Full");
+        }
         passengers.add(passenger);
+        
+       
         
     }
     public void removePassenger(Customer passenger)throws FlightBookingSystemException{
