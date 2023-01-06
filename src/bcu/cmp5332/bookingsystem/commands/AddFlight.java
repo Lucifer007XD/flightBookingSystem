8bcff768b15bd33ebee682b267731b849543a274
Lucifer007XD <115782887+Lucifer007XD@users.noreply.github.com>
2023-01-06 23:12:41 +0000

    Update AddFlight.java

--- src/bcu/cmp5332/bookingsystem/commands/AddFlight.java
+++ src/bcu/cmp5332/bookingsystem/commands/AddFlight.java
@@ -4,19 +4,23 @@ import bcu.cmp5332.bookingsystem.main.FlightBookingSystemException;
 import bcu.cmp5332.bookingsystem.model.Flight;
 import bcu.cmp5332.bookingsystem.model.FlightBookingSystem;
 import java.time.LocalDate;
-
+import java.time.Period;
 public class AddFlight implements  Command {
 
     private final String flightNumber;
     private final String origin;
     private final String destination;
     private final LocalDate departureDate;
+    private double price;
+    private final int capacity;
 
-    public AddFlight(String flightNumber, String origin, String destination, LocalDate departureDate) {
+    public AddFlight(String flightNumber, String origin, String destination, LocalDate departureDate,double price,int capacity) {
         this.flightNumber = flightNumber;
         this.origin = origin;
         this.destination = destination;
         this.departureDate = departureDate;
+        this.price=price;
+        this.capacity=capacity;
     }
     
     @Override
@@ -26,8 +30,19 @@ public class AddFlight implements  Command {
             int lastIndex = flightBookingSystem.getFlights().size() - 1;
             maxId = flightBookingSystem.getFlights().get(lastIndex).getId();
         }
+        LocalDate today=LocalDate.now();
+        Period diff=Period.between(today, this.departureDate);
+        if(diff.getDays()==3) {
+        	this.price=this.price+(0.03*this.price);
+        }else if(diff.getDays()==2) {
+        	this.price=this.price+(0.05*this.price);
+        }else if(diff.getDays()==1) {
+        	this.price=this.price+(0.1*this.price);
+        }else if(diff.getDays()==0) {
+        	this.price=this.price+(0.15*this.price);
+        }
         
-        Flight flight = new Flight(++maxId, flightNumber, origin, destination, departureDate);
+        Flight flight = new Flight(++maxId, flightNumber, origin, destination, departureDate,price,capacity);
         flightBookingSystem.addFlight(flight);
         System.out.println("Flight #" + flight.getId() + " added.");
     }
