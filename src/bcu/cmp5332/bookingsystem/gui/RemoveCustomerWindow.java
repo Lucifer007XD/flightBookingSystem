2e82c8714598dd2ffc31f2dd34e05407f62e198c
Lucifer007XD <115782887+Lucifer007XD@users.noreply.github.com>
2023-01-07 12:57:28 +0000

    Add files via upload

---
+++ src/bcu/cmp5332/bookingsystem/gui/RemoveCustomerWindow.java
@@ -0,0 +1,87 @@
+package bcu.cmp5332.bookingsystem.gui;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
+import bcu.cmp5332.bookingsystem.commands.Command;
+import bcu.cmp5332.bookingsystem.commands.RemoveCustomer;
+import bcu.cmp5332.bookingsystem.main.FlightBookingSystemException;
+
+public class RemoveCustomerWindow extends JFrame implements ActionListener{
+	private MainWindow mw;
+    private JTextField customerIdText = new JTextField();
+   
+    private JButton delBtn = new JButton("Delete");
+    private JButton cancelBtn = new JButton("Cancel");
+    
+    public RemoveCustomerWindow(MainWindow mw) {
+        this.mw = mw;
+        initialize();
+    }
+    public void initialize() {
+    	 try {
+             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+         } catch (Exception ex) {
+
+         }
+
+         setTitle("Delete a Existing Flight");
+
+         setSize(350, 220);
+         JPanel topPanel = new JPanel();
+         topPanel.setLayout(new GridLayout(3, 2));
+         topPanel.add(new JLabel("Customer Id : "));
+         topPanel.add(customerIdText);
+         
+         JPanel bottomPanel = new JPanel();
+         bottomPanel.setLayout(new GridLayout(1, 3));
+         bottomPanel.add(new JLabel("     "));
+         bottomPanel.add(delBtn);
+         bottomPanel.add(cancelBtn);
+
+         delBtn.addActionListener(this);
+         cancelBtn.addActionListener(this);
+
+         this.getContentPane().add(topPanel, BorderLayout.CENTER);
+         this.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+         setLocationRelativeTo(mw);
+
+         setVisible(true);
+    	
+    }
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == delBtn) {
+            delBook();
+        } else if (ae.getSource() == cancelBtn) {
+            this.setVisible(false);
+        }
+
+    }
+
+    private void delBook() {
+        try {
+            int customerId = Integer.parseInt(customerIdText.getText());
+            
+            Command removeCustomer = new RemoveCustomer(customerId);
+            removeCustomer.execute(mw.getFlightBookingSystem());
+            // refresh the view with the list of flights
+            mw.displayCustomers();
+            // hide (close) the AddFlightWindow
+            this.setVisible(false);
+        } catch (FlightBookingSystemException ex) {
+            JOptionPane.showMessageDialog(this, ex, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+}
---
+++ src/bcu/cmp5332/bookingsystem/gui/RemoveFlightWindow.java
@@ -0,0 +1,89 @@
+package bcu.cmp5332.bookingsystem.gui;
+
+import bcu.cmp5332.bookingsystem.commands.RemoveFlight;
+
+import bcu.cmp5332.bookingsystem.commands.Command;
+import bcu.cmp5332.bookingsystem.main.FlightBookingSystemException;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
+public class RemoveFlightWindow extends JFrame implements ActionListener{
+	private MainWindow mw;
+    private JTextField flightIdText = new JTextField();
+   
+    private JButton delBtn = new JButton("Delete");
+    private JButton cancelBtn = new JButton("Cancel");
+    
+    public RemoveFlightWindow(MainWindow mw) {
+        this.mw = mw;
+        initialize();
+    }
+    public void initialize() {
+    	 try {
+             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+         } catch (Exception ex) {
+
+         }
+
+         setTitle("Delete a Existing Flight");
+
+         setSize(350, 220);
+         JPanel topPanel = new JPanel();
+         topPanel.setLayout(new GridLayout(3, 2));
+         topPanel.add(new JLabel("Flight Id : "));
+         topPanel.add(flightIdText);
+         
+         JPanel bottomPanel = new JPanel();
+         bottomPanel.setLayout(new GridLayout(1, 3));
+         bottomPanel.add(new JLabel("     "));
+         bottomPanel.add(delBtn);
+         bottomPanel.add(cancelBtn);
+
+         delBtn.addActionListener(this);
+         cancelBtn.addActionListener(this);
+
+         this.getContentPane().add(topPanel, BorderLayout.CENTER);
+         this.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+         setLocationRelativeTo(mw);
+
+         setVisible(true);
+    	
+    }
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == delBtn) {
+            delBook();
+        } else if (ae.getSource() == cancelBtn) {
+            this.setVisible(false);
+        }
+
+    }
+
+    private void delBook() {
+        try {
+            int flightId = Integer.parseInt(flightIdText.getText());
+            
+            Command removeFlight = new RemoveFlight(flightId);
+            removeFlight.execute(mw.getFlightBookingSystem());
+            // refresh the view with the list of flights
+            mw.displayFlights();
+            // hide (close) the AddFlightWindow
+            this.setVisible(false);
+        } catch (FlightBookingSystemException ex) {
+            JOptionPane.showMessageDialog(this, ex, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+}
