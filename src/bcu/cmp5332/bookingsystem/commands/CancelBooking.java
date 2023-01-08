f70815cc60cf90411deb4ff56d7b6cecb4d7871a
Lucifer007XD <115782887+Lucifer007XD@users.noreply.github.com>
2023-01-08 21:47:19 +0000

    Update CancelBooking.java

--- src/bcu/cmp5332/bookingsystem/commands/CancelBooking.java
+++ src/bcu/cmp5332/bookingsystem/commands/CancelBooking.java
@@ -1,6 +1,8 @@
 package bcu.cmp5332.bookingsystem.commands;
 
 
+import java.time.LocalDate;
+
 import bcu.cmp5332.bookingsystem.main.FlightBookingSystemException;
 
 import bcu.cmp5332.bookingsystem.model.Customer;
@@ -10,6 +12,7 @@ import bcu.cmp5332.bookingsystem.model.FlightBookingSystem;
 public class CancelBooking implements Command {
 	private int customerId;
 	private int flightId;
+	private LocalDate today=LocalDate.now();
 	
 	public CancelBooking(int customerId,int flightId) {
 		this.customerId=customerId;
@@ -20,9 +23,14 @@ public class CancelBooking implements Command {
 	public void execute(FlightBookingSystem flightBookingSystem)throws FlightBookingSystemException{
 		Customer customer=flightBookingSystem.getCustomerByID(customerId);
 		Flight flight=flightBookingSystem.getFlightByID(flightId);
+		if (flight.getDepartureDate().isAfter(today)||flight.getDepartureDate().isEqual(today)) {
 		flight.removePassenger(customer);
 		customer.cancelBookingForFlight(flight);
 		System.out.println("Booking Cancelled Successsfully");
+		}else {
+			throw new FlightBookingSystemException("Flight already Departed");
+		}
+		
 	}
 	
 
