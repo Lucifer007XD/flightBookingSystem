f97bce79c441a9abadd1fddaa592f6ce5ba6b064
Lucifer007XD <115782887+Lucifer007XD@users.noreply.github.com>
2023-01-08 21:45:53 +0000

    Update AddBooking.java

--- src/bcu/cmp5332/bookingsystem/commands/AddBooking.java
+++ src/bcu/cmp5332/bookingsystem/commands/AddBooking.java
@@ -20,18 +20,22 @@ public class AddBooking implements Command{
 	public void execute(FlightBookingSystem flightBookingSystem)throws FlightBookingSystemException{
 		int maxId = 0;
 		for (Customer c : flightBookingSystem.getCustomers()) {
-		    if (c.getBookings().size() > 0) {
-		    	maxId = maxId + c.getBookings().size();
-		    }
+			maxId = maxId + c.getBookings().size();
 		}
 		Customer customer=flightBookingSystem.getCustomerByID(customerId);
 		Flight flight=flightBookingSystem.getFlightByID(flightId);
+		
+		if (bookingDate.isBefore(flight.getDepartureDate())||bookingDate.isEqual(flight.getDepartureDate())) {
 		Booking booking=new Booking(++maxId,customer,flight,bookingDate);
+		
 		
 	
 		customer.addBooking(booking);
 		flight.addPassenger(customer);
-		System.out.println("Booking Issued Successsfully");
+		System.out.println("Booking Issued Successsfully with Booking Id: "+booking.getId()+" for customer: "+customer.getName()+" flight number:"+flight.getFlightNumber());
+		}else {
+			throw new FlightBookingSystemException("Flight has already Departed");
+		}
 	}
 
 }
